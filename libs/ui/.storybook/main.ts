c91f669bdcab879bf89f2d804c8c242f37bfa8b7
Alexey Samofalov <samofalov91@gmail.com>
2025-09-17 17:54:49 +0300

    chore: Storybook was added

---
+++ libs/ui/.storybook/main.ts
@@ -0,0 +1,11 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+};
+export default config;
